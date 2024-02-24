7a4eac288f7d9944fb96891a3e61591ca1d583c0
tomakor549 <32541866+tomakor549@users.noreply.github.com>
2024-02-24 17:09:34 +0100

    Add project files.

---
+++ 1.GettingStarted/Movies.Api/Controllers/MoviesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movies.Application.Repositories;
+using Movies.Contracts.Requests;
+
+namespace Movies.Api.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class MoviesController : ControllerBase
+    {
+        private readonly IMovieRepository _movieRepository;
+
+        public MoviesController(IMovieRepository movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
+
+        [HttpPost("movies")]
+        public async Task<IActionResult> Create([FromBody]CreateMovieRequest request)
+        {
+            return Ok(request);
+        }
+    }
+}
