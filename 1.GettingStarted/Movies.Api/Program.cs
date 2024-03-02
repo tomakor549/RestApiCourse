43e95ea516142a97043c927bf630ac81929c21d7
tomakor549 <venetor7@gmail.com>
2024-03-02 15:33:50 +0100

    Add: Database connection

--- 1.GettingStarted/Movies.Api/Program.cs
+++ 1.GettingStarted/Movies.Api/Program.cs
@@ -1,4 +1,5 @@
 using Movies.Application;
+using Movies.Application.Database;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,8 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+var config = builder.Configuration;
 
 builder.Services.AddApplication();
+builder.Services.AddDatabase(config["Database:ConnectionString"]!);
 
 var app = builder.Build();
 
@@ -25,5 +28,8 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+var dbInitializer = app.Services.GetRequiredService<DbInitializer>();
+await dbInitializer.InitializeAsync();
 
 app.Run();
--- 1.GettingStarted/Movies.Application/ApplicationServiceCollectionExtensions.cs
+++ 1.GettingStarted/Movies.Application/ApplicationServiceCollectionExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System.Data;
+using Microsoft.Extensions.DependencyInjection;
+using Movies.Application.Database;
 using Movies.Application.Repositories;
 
 namespace Movies.Application
@@ -8,6 +10,14 @@ namespace Movies.Application
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
             services.AddSingleton<IMovieRepository, MovieRepository>();
+            return services;
+        }
+
+        public static IServiceCollection AddDatabase(this IServiceCollection services, string connectionString)
+        {
+            services.AddSingleton<IDbConnectionFactory>(_ => 
+                new NpgsqlConnectionFactory(connectionString));
+            services.AddSingleton<DbInitializer>();
             return services;
         }
     }
